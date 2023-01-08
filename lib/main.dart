139359ba4b99bc105ca28224268210bd509d8cc3
ryanfigo <ryanfigo12@gmail.com>
2023-01-08 16:24:45 +0800

    mobile

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:toko/users/authentication/login_screen.dart';
+import 'package:toko/users/fragments/dashboard_of_fragments.dart';
+import 'package:toko/users/userPreferences/user_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'I Decide',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: FutureBuilder(
+        future: RememberUserPrefs.readUserInfo(),
+        builder: (context, dataSnapShot) {
+          if (dataSnapShot.data == null) {
+            return LoginScreen();
+          } else {
+            return DashboardOfFragments();
+          }
+        },
+      ),
+    );
+  }
+}
